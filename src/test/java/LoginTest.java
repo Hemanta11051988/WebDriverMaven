4ae99536df6de976c508855885dcd97e3b848ccd
hemanta kumar nayak <hnayak23@yahoo.com>
2019-09-27 18:19:36 +0530

    commiting the changes

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -19,7 +21,8 @@ public class LoginTest {
 		driver.get("http://gmail.com");
 		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("Hemantamechanical@gmail.com");
 		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span/span")).click();
-
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.name("password")).sendKeys("HemantapASSWORD");
 	}
 
 	@AfterSuite
