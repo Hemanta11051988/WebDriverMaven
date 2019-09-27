00bb43070baf671043bd15196949fa17ade1539a
hemanta kumar nayak <hnayak23@yahoo.com>
2019-09-27 18:07:10 +0530

    first commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void SetUp() {
+		driver = new FirefoxDriver();
+	}
+
+	@Test
+	public void doLogin() {
+		driver.get("http://gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("Hemantamechanical@gmail.com");
+		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span/span")).click();
+
+	}
+
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+}
